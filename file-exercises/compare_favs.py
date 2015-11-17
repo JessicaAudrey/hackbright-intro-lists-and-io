bf4f2e4923e72a4e09aee965e5b2983d6eb0e687
Jessica <jessica.clayton.a@gmail.com>
2015-11-16 20:11:32 -0800

    added a comparison foods python file

---
+++ file-exercises/compare_favs.py
@@ -0,0 +1,10 @@
+my_file = open("jessica_fav_foods.txt")
+JessicaFood = my_file.readlines()
+print JessicaFood
+my_file.close ()
+
+my_file2 = open("Alexis_fav_foods.txt")
+AlexisFood = my_file2.readlines()
+print AlexisFood
+my_file2.close ()
+
